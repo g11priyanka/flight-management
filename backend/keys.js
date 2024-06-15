091938724fbd99c0cbd16669cf6a15b49917c82b
g11priyanka <168571780+g11priyanka@users.noreply.github.com>
2024-06-16 01:03:24 +0530

    commit

---
+++ backend/keys.js
@@ -0,0 +1,10 @@
+module.exports={
+    MONGOURI: "mongodb+srv://priyanka:4LYSwzvU1AYdI4hB@cluster0.w6zxxas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    JWT_SECRET:"qsxcefbyhjmolpknuy"
+}
+
+
+
+
+
+//4LYSwzvU1AYdI4hB
